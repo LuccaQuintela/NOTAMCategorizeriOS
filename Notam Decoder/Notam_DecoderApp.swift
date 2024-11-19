470cefb501ed106477a28fa7f019525fcf4e9d2e
Lucca Quintela <lucca.b.quintela@gmail.com>
2024-11-19 12:28:29 -0700

    Initial Commit

---
+++ Notam Decoder/Notam_DecoderApp.swift
@@ -0,0 +1,17 @@
+//
+//  Notam_DecoderApp.swift
+//  Notam Decoder
+//
+//  Created by Lucca Quintela on 11/19/24.
+//
+
+import SwiftUI
+
+@main
+struct Notam_DecoderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
